63f407b139bfb71b4d261c232cd625df32dc7d4c
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2023-04-23 18:21:19 +0700

    profiles migration

---
+++ database/migrations/2023_04_22_181341_create_profiles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('full_name');
+            $table->enum('gender', ['M', 'F']);
+            $table->timestampTz('date_of_birth');
+            $table->string('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
