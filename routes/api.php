d2010b824618aff351b6cd2eb9b6579ff33a4e04
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2023-04-23 18:21:39 +0700

    route login, regist, and show authenticated user

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthenticationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::get('/user', function (Request $request) {
+//     return response()->json($request->user());
+// });
+Route::get('user', [AuthenticationController::class, 'show']);
+
+Route::post('users', [AuthenticationController::class, 'register']);
+Route::get('users', [AuthenticationController::class, 'login']);
