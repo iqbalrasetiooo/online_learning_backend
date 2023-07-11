8e452cec7c966bfa50ddaf710e4f13b6196ef8c0
Iqbal Rasetio <rasetioiqbal@gmail.com>
2023-07-11 15:04:23 +0700

    merge incoming change api.php

--- routes/api.php
+++ routes/api.php
@@ -23,5 +23,33 @@ use App\Http\Controllers\API\VideoController;
 //     return response()->json($request->user());
 // });
 
-Route::post('users', [AuthenticationController::class, 'register']);
-Route::post('users', [AuthenticationController::class, 'login']);
+//get all user
+Route::get('user', [AuthenticationController::class, 'index']);
+//get user by id
+Route::get('user/{id}', [AuthenticationController::class, 'show']);
+//update user
+Route::put('user/{id}', [AuthenticationController::class, 'updateUser']);
+//register
+Route::post('register', [AuthenticationController::class, 'register']);
+Route::post('register/lecturer', [AuthenticationController::class, 'registerLecturer']);
+//login
+Route::post('login', [AuthenticationController::class, 'login']);
+//get all lecturer
+Route::get('lecturer', [LecturerController::class, 'index']);
+//add lecturer
+Route::post('lecturer/add', [LecturerController::class, 'createLecturer']);
+//get all course
+Route::get('course', [CourseController::class, 'index']);
+//get course by id
+Route::get('course/{id}', [CourseController::class, 'show']);
+//delete course 
+Route::delete('course/{id}',[CourseController::class, 'destroy']);
+//add course
+Route::post('course/add', [CourseController::class, 'store']);
+//get categories
+Route::get('categories', [CategoryController::class, 'index']);
+//add categories
+Route::post('course/category', [CategoryController::class, 'addCategory']);
+
+Route::post('video/add', [VideoController::class, 'store']);
+Route::delete('video/delete/{id}', [VideoController::class, 'destroy']);
